46c90fbd3f731627c00cc895cc308877e198611b
Rakesh Baruah <rbaruah@gmail.com>
2023-04-07 12:38:01 -0500

    add login / logout logic to home view

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(100);
